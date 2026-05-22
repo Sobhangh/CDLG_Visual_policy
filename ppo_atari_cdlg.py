9a3fca363886907abf963bc52d6918ec0cc1b876
Sobhangh <sobhan.gharanfoli@gmail.com>
2026-05-22 09:28:50 +0200

    update

--- ppo_atari_cdlg.py
+++ ppo_atari_cdlg.py
@@ -77,7 +77,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "PongNoFrameskip-v4"
     """the id of the environment"""
-    total_timesteps: int = 2_000_000 #10000000
+    total_timesteps: int = 7_000_000 #10000000
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
