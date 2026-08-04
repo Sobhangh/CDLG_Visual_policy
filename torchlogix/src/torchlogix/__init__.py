5eb7f5001dd99f890e9b4cad1621ecb701c7a665
Sobhangh <sobhan.gharanfoli@gmail.com>
2026-08-04 11:17:14 +0200

    torchlogix update

--- torchlogix/src/torchlogix/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The main package for torchlogix."""
-
-from .compiled_model import CompiledLogicNet
-from .packbitstensor import PackBitsTensor
-from . import layers
-
-__all__ = ["CompiledLogicNet", "PackBitsTensor", "layers"]
